ebc1b6ddf8414676ea63e8210cf2c2cddd4cb871
Luis <gerardo.fosado@cargamos.com>
2025-02-02 18:11:19 -0600

    Remove cached files

--- app/v1/flows/_base_flow.py
+++
@@ -1,6 +0,0 @@
-from infra.mongo import MongoRepository
-
-
-class Flow:
-    def __init__(self, repository: MongoRepository):
-        self._repository = repository
--- app/v1/flows/users.py
+++
@@ -1,5 +0,0 @@
-from app.v1.flows._base_flow import Flow
-
-
-class UserFlow(Flow):
-    pass
